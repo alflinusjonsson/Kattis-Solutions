d443a5d4f0d904d9a5ca37c6830c8df88be36437
Linus Jönsson <alflinusjonsson@gmail.com>
2021-05-16 10:01:23 +0200

    Solved FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+	int x, y, n;
+	std::cin >> x >> y >> n;
+
+	for (int i = 1; i < n+1; i++) {
+		if ( ((i % x) == 0) && ((i % y) == 0) ) {
+			std::cout << "FizzBuzz" << std::endl;
+		}
+		else if ((i % x) == 0) {
+			std::cout << "Fizz" << std::endl;
+		}
+		else if ((i % y) == 0) {
+			std::cout << "Buzz" << std::endl;
+		}
+		else {
+			std::cout << i << std::endl;
+		}
+	}
+}
